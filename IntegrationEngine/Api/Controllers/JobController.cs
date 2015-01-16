c90aa9e6c07c67a84633fc649e2e85d85bfd998d
Ethan Hann <ethanhann@gmail.com>
2015-01-15 21:08:31 -0500

    Add JobController

---
+++ IntegrationEngine/Api/Controllers/JobController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using IntegrationEngine.Core.Storage;
+using IntegrationEngine.Model;
+using IntegrationEngine.Scheduler;
+using Quartz.Impl.Matchers;
+using Quartz;
+
+namespace IntegrationEngine.Api.Controllers
+{
+    public class JobController : ApiController
+    {
+        public IEngineScheduler EngineScheduler { get; set; }
+
+        public JobController()
+        {
+        }
+
+        public JobController(IEngineScheduler engineScheduler)
+            : this()
+        {
+            EngineScheduler = engineScheduler;
+        }
+
+        public IHttpActionResult GetJobs()
+        {
+            return Ok(EngineScheduler.Scheduler.GetJobKeys(GroupMatcher<JobKey>.AnyGroup()).ToList());
+        }
+
+    }
+}
