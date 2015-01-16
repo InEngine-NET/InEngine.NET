41b1b25c14f8a8be264b4972fe9937fb9299d871
Ethan Hann <eh2413@columbia.edu>
2015-01-16 12:12:27 -0500

    Use correct MySql.Data.Entity dependency in package

--- configuration/SharedAssemblyInfo.cs
+++ configuration/SharedAssemblyInfo.cs
@@ -3,7 +3,7 @@
 // Assembly Info that is shared across the product
 [assembly: AssemblyProduct("InEngine.NET")]
 [assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyInformationalVersion("2.0.0-alpha3")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha4")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ethan Hann")]
