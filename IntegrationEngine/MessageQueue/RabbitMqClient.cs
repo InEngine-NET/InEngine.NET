184a6b3f74ccc8988cd86ce0218ae69e46d89aab
Ethan Hann <ethanhann@gmail.com>
2015-01-17 13:06:07 -0500

    Log exception when publishing message to queue

--- IntegrationEngine/MessageQueue/RabbitMqClient.cs
+++ IntegrationEngine/MessageQueue/RabbitMqClient.cs
@@ -16,15 +16,22 @@ namespace IntegrationEngine.MessageQueue
 
         public void Publish<T>(T value)
         {
-            var type = value.GetType();
-            var message = String.Join(".", type.Namespace, type.Name);
-            var connection = MessageQueueConnection.GetConnection();
-            using (var channel = connection.CreateModel())
+            try
             {
-                channel.QueueBind(MessageQueueConfiguration.QueueName, MessageQueueConfiguration.ExchangeName, "");
-                var body = Encoding.UTF8.GetBytes(message);
-                channel.BasicPublish(MessageQueueConfiguration.ExchangeName, "", null, body);
-                Log.Info(string.Format("Sent {0}", message));
+                var type = value.GetType();
+                var message = type.FullName;
+                var connection = MessageQueueConnection.GetConnection();
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueBind(MessageQueueConfiguration.QueueName, MessageQueueConfiguration.ExchangeName, "");
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(MessageQueueConfiguration.ExchangeName, "", null, body);
+                    Log.Debug(x => x("Sent message: {0}", message));
+                }
+            }
+            catch (Exception exception)
+            {
+                Log.Error(exception);
             }
         }
     }
