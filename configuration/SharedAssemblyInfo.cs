5ce524c1a05294516d49490bc5bda4b804a5493c
Ethan Hann <ethanhann@gmail.com>
2015-01-18 22:37:43 -0500

    Bump version

--- configuration/SharedAssemblyInfo.cs
+++ configuration/SharedAssemblyInfo.cs
@@ -3,7 +3,7 @@
 // Assembly Info that is shared across the product
 [assembly: AssemblyProduct("InEngine.NET")]
 [assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyInformationalVersion("2.0.0-beta1")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta2")]
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ethan Hann")]
