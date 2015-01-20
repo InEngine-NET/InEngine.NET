71a2130022d71f349b47c6e8101f6e7412867ad7
Ethan Hann <eh2413@columbia.edu>
2015-01-20 14:35:09 -0500

    Add console client skeleton

---
+++ IntegrationEngine.ConsoleClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using IntegrationEngine.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegrationEngine.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new InEngineClient();
+        }
+    }
+}
