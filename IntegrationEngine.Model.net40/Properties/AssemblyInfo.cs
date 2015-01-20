1a82c8a82b1d8abaef4d56fc2476dcf38aad5cfe
Ethan Hann <eh2413@columbia.edu>
2015-01-20 12:43:50 -0500

    Fix assembly name

--- IntegrationEngine.Model.net40/Properties/AssemblyInfo.cs
+++ IntegrationEngine.Model.net40/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IntegrationEngine.Model.Net4")]
+[assembly: AssemblyTitle("IntegrationEngine.Model.net40")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
