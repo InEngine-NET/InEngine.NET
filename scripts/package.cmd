376a19773c47c9d31448ed06076dc345d621e3d1
Ethan Hann <ethanhann@gmail.com>
2015-01-19 23:00:45 -0500

    Add client to package script

--- scripts/package.cmd
+++ scripts/package.cmd
@@ -1,4 +1,5 @@
 rm *.nupkg
 %userprofile%\bin\nuget.exe pack IntegrationEngine.Model\IntegrationEngine.Model.csproj -Prop Configuration=Release -Prop Platform=AnyCPU
+%userprofile%\bin\nuget.exe pack IntegrationEngine.Client\IntegrationEngine.Client.csproj -Prop Configuration=Release -Prop Platform=AnyCPU
 %userprofile%\bin\nuget.exe pack IntegrationEngine.Core\IntegrationEngine.Core.csproj -Prop Configuration=Release -Prop Platform=AnyCPU
 %userprofile%\bin\nuget.exe pack IntegrationEngine\IntegrationEngine.csproj -Prop Configuration=Release -Prop Platform=AnyCPU
