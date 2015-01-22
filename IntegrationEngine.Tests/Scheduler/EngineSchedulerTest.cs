80bf7489f78cbce15cc55eee3732caad97f1c412
Ethan Hann <ethanhann@gmail.com>
2015-01-22 00:13:13 -0500

    Setup for scheduler tests

---
+++ IntegrationEngine.Tests/Scheduler/EngineSchedulerTest.cs
@@ -0,0 +1,17 @@
+﻿using IntegrationEngine.Scheduler;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+
+namespace IntegrationEngine.Tests.Scheduler
+{
+    public class EngineSchedulerTest
+    {
+        public void ShouldDeleteTrigger()
+        {
+
+
+
+        }
+    }
+}
