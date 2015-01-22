858b92309d5b7b43d8eca5e8296ac29bc44c8702
Ethan Hann <ethanhann@gmail.com>
2015-01-22 18:26:26 -0500

    Add implementations

---
+++ IntegrationEngine/MessageQueue/IMessageQueueListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationEngine.MessageQueue
+{
+    public interface IMessageQueueListener
+    {
+        void Listen();
+    }
+}
---
+++ IntegrationEngine/MessageQueue/MsmqClient.cs
@@ -0,0 +1,53 @@
+﻿using Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MSMessageQueue = System.Messaging.MessageQueue;
+
+namespace IntegrationEngine.MessageQueue
+{
+    public class MsmqClient : IMessageQueueClient
+    {
+        public MSMessageQueue MSMessageQueue { get; set; }
+        string _queueName { get; set; }
+        public string QueueName { 
+            get { return _queueName; }
+            set {
+                if (!MSMessageQueue.Exists(QueueName))
+                    MSMessageQueue.Create(QueueName);
+                _queueName = value;
+            }
+        }
+        public ILog Log { get; set; }
+
+        public void Publish<T>(T value)
+        {
+            try
+            {
+                var message = value.GetType().FullName;
+                var body = Encoding.UTF8.GetBytes(message);
+                MSMessageQueue.Send(body);
+                Log.Debug(x => x("Sent message: {0}", message));
+            }
+            catch (Exception exception)
+            {
+                Log.Error(exception);
+            }
+        }
+
+        public bool IsServerAvailable()
+        {
+            try
+            {
+                return MSMessageQueue.Exists(QueueName);
+            }
+            catch (Exception exception)
+            {
+                Log.Error(exception);
+                return false;
+            }
+        }
+    }
+}
