68b523e27ac249ab7914d4e13c1ed0dc73e773c4
Ethan Hann <ethanhann@gmail.com>
2015-01-24 13:10:57 -0500

    Add more tests

--- IntegrationEngine.Tests/Api/Controllers/JobTypeControllerTest.cs
+++ IntegrationEngine.Tests/Api/Controllers/JobTypeControllerTest.cs
@@ -5,6 +5,7 @@ using IntegrationEngine.Scheduler;
 using Moq;
 using NUnit.Framework;
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -17,18 +18,18 @@ namespace IntegrationEngine.Tests
         {
             var engineScheduler = new Mock<EngineScheduler>();
             var type = typeof(IntegrationJobFixture);
-            var expected = new List<JobType>() {
-                new JobType() {
-                    FullName = type.FullName,
-                    Name = type.Name,
-                }
+            var expected = new JobType() {
+                FullName = type.FullName,
+                Name = type.Name,
             };
             engineScheduler.SetupGet(x => x.IntegrationJobTypes).Returns(new List<Type>() { type });
             Subject.EngineScheduler = engineScheduler.Object;
 
             var result = Subject.GetJobTypes();
 
-            Assert.That(result, Is.EqualTo(expected));
+            var first = result.First();
+            Assert.That(first.FullName, Is.EqualTo(expected.FullName));
+            Assert.That(first.Name, Is.EqualTo(expected.Name));
         }
     }
 }
