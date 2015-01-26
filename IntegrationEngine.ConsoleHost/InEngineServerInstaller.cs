10dd5c457a076a48d7239ebd4a39a9f8e62e7414
Ethan Hann <eh2413@columbia.edu>
2015-01-26 13:30:32 -0500

    Support installing as service

---
+++ IntegrationEngine.ConsoleHost/InEngineServerInstaller.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+
+namespace IntegrationEngine.ConsoleHost
+{
+    [RunInstaller(true)]
+    public class InEngineServerInstaller : Installer
+    {
+        public InEngineServerInstaller()
+        {
+            var serviceProcessInstaller = new ServiceProcessInstaller();
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            serviceProcessInstaller.Username = null;
+            serviceProcessInstaller.Password = null;
+            Installers.Add(serviceProcessInstaller);
+
+            var serviceInstaller = new ServiceInstaller();
+            serviceInstaller.DisplayName = "InEngine.NET Server";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+            serviceInstaller.ServiceName = Program.ServiceName;
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
