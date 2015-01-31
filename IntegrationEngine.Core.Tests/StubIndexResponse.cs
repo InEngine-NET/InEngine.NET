ef6bde3459aaf1e8dbbbfb35ab85c75083d2d4d9
Ethan Hann <ethanhann@gmail.com>
2015-01-31 16:33:26 -0500

    Add stub for IndexResponse

---
+++ IntegrationEngine.Core.Tests/StubIndexResponse.cs
@@ -0,0 +1,89 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using System;
+
+namespace IntegrationEngine.Core.Tests
+{
+    public class StubIndexResponse : IIndexResponse
+    {
+        public StubIndexResponse()
+        {
+        }
+
+        #region IIndexResponse implementation
+
+        public string Id {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Index {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Type {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Version {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool Created {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+
+        #region IResponse implementation
+
+        public bool IsValid {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IElasticsearchResponse ConnectionStatus {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ElasticInferrer Infer {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ElasticsearchServerError ServerError {
+            get {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+
+        #region IResponseWithRequestInformation implementation
+
+        public IElasticsearchResponse RequestInformation {
+            get {
+                throw new NotImplementedException();
+            }
+            set {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion
+    }
+}
+
