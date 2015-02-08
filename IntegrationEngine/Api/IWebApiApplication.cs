2673080b4f3b414838e397261bc4c567dbb72919
Ethan Hann <ethanhann@gmail.com>
2015-02-07 23:37:05 -0500

    Refactor WebAPi app

---
+++ IntegrationEngine/Api/IWebApiApplication.cs
@@ -0,0 +1,13 @@
+﻿using IntegrationEngine.Configuration;
+using System;
+
+namespace IntegrationEngine.Api
+{
+    public interface IWebApiApplication
+    {
+        IDisposable webApi { get; set; }
+        WebApiConfiguration WebApiConfiguration { get; set; }
+        void Start();
+        void Stop();
+    }
+}
---
+++ IntegrationEngine/Api/WebApiStartup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Microsoft.Practices.Unity;
+using IntegrationEngine.Configuration;
+
+[assembly: OwinStartup(typeof(IntegrationEngine.Api.WebApiStartup))]
+
+namespace IntegrationEngine.Api
+{
+    public class WebApiStartup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var  config = new HttpConfiguration();
+            var container = ContainerSingleton.GetContainer();
+            config.DependencyResolver = new ContainerResolver(container);
+            var webApiConfig = container.Resolve<EngineConfiguration>().WebApi;
+            if (webApiConfig.Origins.Any())
+                config.EnableCors(new EnableCorsAttribute(string.Join(",", webApiConfig.Origins), "*", "*"));
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.MapHttpAttributeRoutes();
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
