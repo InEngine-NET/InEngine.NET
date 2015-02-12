8afb8b26f210f6bd652ec251cdbab3b8aacc1530
Ethan Hann <ethanhann@gmail.com>
2015-02-12 13:12:29 -0500

    Fix assembly name in Client.net40

--- IntegrationEngine.Client.net40/Properties/AssemblyInfo.cs
+++ IntegrationEngine.Client.net40/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IntegrationEngine.Client.net40")]
+[assembly: AssemblyTitle("IntegrationEngine.Client")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
