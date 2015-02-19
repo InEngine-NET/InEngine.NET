73d412c1eb7bd1a33130839c0fb3fe414386a881
Ethan Hann <ethanhann@gmail.com>
2015-02-18 22:53:16 -0500

    Add refactored integration jobs

---
+++ IntegrationEngine.Core/IntegrationJob/IParameterizedJob.cs
@@ -0,0 +1,12 @@
+﻿using IntegrationEngine.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationEngine.Core.IntegrationJob
+{
+    public interface IParameterizedJob : IIntegrationJob, IHasParameters
+    {}
+}
