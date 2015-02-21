bb65ba8d29005091395903bbb1612bd69af135f6
Ethan Hann <ethanhann@gmail.com>
2015-02-21 17:04:04 -0500

    Add tests for web api configuration

---
+++ IntegrationEngine.Tests/Api/WebApiApplicationTest.cs
@@ -0,0 +1,63 @@
+﻿using BeekmanLabs.UnitTesting;
+using IntegrationEngine.Api;
+using IntegrationEngine.Core.Configuration;
+using Microsoft.Owin.Hosting;
+using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Moq;
+using System.Web.Http.Routing;
+
+namespace IntegrationEngine.Tests
+{
+    public class WebApiApplicationTest : TestBase<WebApiApplication>
+    {
+
+        [Test]
+        public void ShouldSetDependencyResolver()
+        {
+            var expected = new ContainerResolver();
+            Subject.ContainerResolver = expected;
+
+            var httpConfiguration = Subject.HttpConfigurationFactory();
+
+            Assert.That(httpConfiguration.DependencyResolver, Is.SameAs(expected));
+        }
+
+        [Test]
+        public void ShouldConfigureCors()
+        {
+            var webApiConfiguration = new WebApiConfiguration() {
+                Origins = new List<string> { "*" },
+            };
+            Subject.WebApiConfiguration = webApiConfiguration;
+            Subject.ContainerResolver = new ContainerResolver();
+
+            var httpConfiguration = Subject.HttpConfigurationFactory();
+
+            var policyProviderFactory = (httpConfiguration.GetCorsPolicyProviderFactory() as AttributeBasedPolicyProviderFactory);
+            Assert.That(policyProviderFactory.DefaultPolicyProvider, Is.Not.Null);
+        }
+
+        [Test]
+        public void ShouldConfigureRoutes()
+        {
+            var expected = new ContainerResolver();
+            Subject.ContainerResolver = expected;
+
+            var httpConfiguration = Subject.HttpConfigurationFactory();
+
+            Assert.That(httpConfiguration.Routes.Count, Is.EqualTo(2));
+            IHttpRoute defaultHttpRoute;
+            httpConfiguration.Routes.TryGetValue("DefaultApi", out defaultHttpRoute);
+            Assert.That(defaultHttpRoute.RouteTemplate, Is.EqualTo("api/{controller}/{id}"));
+            Assert.That(defaultHttpRoute.Defaults.Keys.Contains("id"), Is.True);
+            Assert.That(httpConfiguration.Routes.ContainsKey("MS_attributerouteWebApi"), Is.True);
+        }
+    }
+}
--- IntegrationEngine/Api/WebApiApplication.cs
+++ IntegrationEngine/Api/WebApiApplication.cs
@@ -1,11 +1,11 @@
 ﻿using IntegrationEngine.Core.Configuration;
 using Microsoft.Owin.Hosting;
-using System;
-using System.Web.Http;
 using Microsoft.Practices.Unity;
-using System.Linq;
-using System.Web.Http.Cors;
 using Owin;
+using System;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace IntegrationEngine.Api
 {
@@ -18,23 +18,25 @@ namespace IntegrationEngine.Api
         public WebApiApplication()
         {}
 
+        public HttpConfiguration HttpConfigurationFactory()
+        {
+            var  config = new HttpConfiguration();
+            config.DependencyResolver = ContainerResolver;
+            if (WebApiConfiguration != null && WebApiConfiguration.Origins.Any())
+                config.EnableCors(new EnableCorsAttribute(string.Join(",", WebApiConfiguration.Origins), "*", "*"));
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.MapHttpAttributeRoutes();
+            return config;
+        }
+
         public void Start()
         {
             var baseAddress = (new UriBuilder("http", WebApiConfiguration.HostName, WebApiConfiguration.Port)).Uri.AbsoluteUri;
-            Action<Owin.IAppBuilder> startup = x => {
-                var  config = new HttpConfiguration();
-                config.DependencyResolver = ContainerResolver;
-//                if (WebApiConfiguration != null && WebApiConfiguration.Origins.Any())
-//                    config.EnableCors(new EnableCorsAttribute(string.Join(",", WebApiConfiguration.Origins), "*", "*"));
-                config.Routes.MapHttpRoute(
-                    name: "DefaultApi",
-                    routeTemplate: "api/{controller}/{id}",
-                    defaults: new { id = RouteParameter.Optional }
-                );
-                config.MapHttpAttributeRoutes();
-                x.UseWebApi(config);
-            };
-            webApi = WebApp.Start(baseAddress, startup);
+            webApi = WebApp.Start(baseAddress, x => x.UseWebApi(HttpConfigurationFactory()));
         }
 
         public void Dispose()
