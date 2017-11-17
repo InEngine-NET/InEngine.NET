c82ba2719d16a3187159ad5c9a68255d22feeb3c
Ethan Hann <ethanhann@gmail.com>
2017-11-16 22:34:46 -0500

    Turn off logging by default, centralize assembly info

---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+
+// Assembly Info that is shared across the product
+[assembly: AssemblyProduct("InEngine.NET")]
+[assembly: AssemblyVersion("3.0.0.*")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ethan Hann")]
+[assembly: AssemblyCopyright("Copyright © Ethan Hann 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("")]
