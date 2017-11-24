c3b09f965cde081e29b017ba874d6f9c6d5eff08
Ethan Hann <ethanhann@gmail.com>
2017-11-24 00:00:07 -0500

    Refactor/rename queue flush command

--- src/InEngine.Core/Queue/Commands/Flush.cs
+++ src/InEngine.Core/Queue/Commands/Flush.cs
@@ -6,27 +6,27 @@ namespace InEngine.Core.Queue.Commands
     public class Flush : AbstractCommand
     {
         [Option("pending", HelpText = "Clear the pending queue.")]
-        public bool ClearPendingQueue { get; set; }
+        public bool PendingQueue { get; set; }
 
         [Option("failed", HelpText = "Clear the failed queue.")]
-        public bool ClearFailedQueue { get; set; }
+        public bool FailedQueue { get; set; }
 
         [Option("in-progress", HelpText = "Clear the in-progress queue.")]
-        public bool ClearInProgressQueue { get; set; }
+        public bool InProgressQueue { get; set; }
 
         [Option("secondary", HelpText = "Clear secondary queues. Primary queues are cleared by default.")]
         public bool UseSecondaryQueue { get; set; }
 
         public override void Run()
         {
-            if (ClearPendingQueue == false && ClearFailedQueue == false && ClearInProgressQueue == false)
+            if (PendingQueue == false && FailedQueue == false && InProgressQueue == false)
                 throw new CommandFailedException("Must specify at least one queue to clear. Use -h to see available options.");
             var broker = Broker.Make(UseSecondaryQueue);
-            if (ClearPendingQueue)
+            if (PendingQueue)
                 Info($"Pending: {broker.ClearPendingQueue().ToString()}");
-            if (ClearInProgressQueue)
+            if (InProgressQueue)
                 Info($"In-progress: {broker.ClearInProgressQueue().ToString()}");
-            if (ClearFailedQueue)
+            if (FailedQueue)
                 Info($"Failed: {broker.ClearFailedQueue().ToString()}");
         }
     }
