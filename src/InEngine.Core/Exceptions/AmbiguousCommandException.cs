2c28cee067db3757d649e292b2aff14aa7db457b
Ethan Hann <ethanhann@gmail.com>
2017-11-24 20:51:10 -0500

    Refine publish command's options

---
+++ src/InEngine.Core/Exceptions/AmbiguousCommandException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace InEngine.Core.Exceptions
+{
+    public class AmbiguousCommandException : Exception
+    {
+        public AmbiguousCommandException(string commandName) 
+            : base($"Command name found in multiple plugins: {commandName}")
+        {
+        }
+    }
+}
---
+++ src/InEngine.Core/Exceptions/CommandNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace InEngine.Core.Exceptions
+{
+    public class CommandNotFoundException : Exception
+    {
+        public CommandNotFoundException(string command) 
+            : base($"Command not found: {command}")
+        {}
+    }
+}
