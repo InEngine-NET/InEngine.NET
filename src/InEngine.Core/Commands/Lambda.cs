8369675b65549a853f1e14904392762c9a3cbbd0
Ethan Hann <ethanhann@gmail.com>
2017-12-01 19:58:00 -0500

    Add a lambda command

---
+++ src/InEngine.Core/Commands/Lambda.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InEngine.Core.Commands
+{
+    public class Lambda : AbstractCommand
+    {
+        public Action Action { get; set; }
+        public override void Run()
+        {
+            Action.Invoke();
+        }
+    }
+}
