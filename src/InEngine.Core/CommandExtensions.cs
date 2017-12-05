4d0599c61df3ec4f49a9725ece130ca7c495e650
Ethan Hann <ethanhann@gmail.com>
2017-12-05 18:24:54 -0500

    Change consume icon

--- src/InEngine.Core/CommandExtensions.cs
+++ src/InEngine.Core/CommandExtensions.cs
@@ -5,7 +5,7 @@ namespace InEngine.Core
     {
         public static void WriteSummaryToConsole(this AbstractCommand command)
         {
-            Console.WriteLine($"- {command.Name} : {command.ScheduleId} | Try #{command.CommandLifeCycle.CurrentTry}");
+            Console.WriteLine($"⚡ {command.Name} : {command.ScheduleId} | Try #{command.CommandLifeCycle.CurrentTry}");
         }
     }
 }
