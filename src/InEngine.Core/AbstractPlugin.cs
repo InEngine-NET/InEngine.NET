397f45ea26ed047ca103546d5b167dfa4d444a6e
Ethan Hann <ethanhann@gmail.com>
2017-12-07 23:31:22 -0500

    Cleanup abstract plugin

--- src/InEngine.Core/AbstractPlugin.cs
+++ src/InEngine.Core/AbstractPlugin.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using CommandLine;
 using CommandLine.Text;
@@ -13,33 +12,11 @@ namespace InEngine.Core
         {}
 
         [HelpVerbOption]
-        public string GetUsage(string verb)
+        public virtual string GetUsage(string verb)
         {
             var helpText = HelpText.AutoBuild(this, verb);
-            helpText.Heading = $"{GetType().Assembly.GetName().Name} v{GetType().Assembly.GetName().Version.ToString()}";
+            helpText.Heading = $"{GetType().Assembly.GetName().Name} {GetType().Assembly.GetName().Version.ToString()}";
             return helpText;
-        }
-
-        public virtual string GetUsageWithoutHeader()
-        {
-            var verbs = GetType()
-                .GetProperties()
-                .ToList()
-                .SelectMany(x => {
-                    return x.GetCustomAttributes(typeof(VerbOptionAttribute), true).Select(y => y as VerbOptionAttribute);
-                });
-            var maxWidth = 0;
-            foreach (var verb in verbs)
-                if (verb.LongName != null && verb.LongName.Length > maxWidth)
-                    maxWidth = verb.LongName.Length;
-
-            var helpTextLine = verbs.Select(x => {
-                var name = (x.LongName ?? "");
-                var padding = maxWidth - name.Length + 2;
-                return $"  {name}" + string.Join("", Enumerable.Range(0, padding).Select(y => " ")) + (x.HelpText ?? "");
-            });
-
-            return string.Join(Environment.NewLine, helpTextLine);
         }
 
         public IList<VerbOptionAttribute> GetVerbOptions()
