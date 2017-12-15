5141e1e79fb260c1edbf9fe5235046a435f48fba
Ethan Hann <ethanhann@gmail.com>
2017-12-15 16:25:33 -0500

    Remove legacy nuget

--- src/InEngine.Core/Queuing/Commands/Length.cs
+++ src/InEngine.Core/Queuing/Commands/Length.cs
@@ -1,4 +1,6 @@
-﻿namespace InEngine.Core.Queuing.Commands
+﻿using System;
+
+namespace InEngine.Core.Queuing.Commands
 {
     public class Length : AbstractCommand
     {
@@ -10,14 +12,45 @@
 
         public void PrintQueueLengths(QueueAdapter queue)
         {
-            var leftPadding = 15;
+            var textLeftPadding = 15;
+            var numberLeftPadding = 2;
             Warning($"{queue.QueueName} Queue:");
-            InfoText("Pending".PadLeft(leftPadding));
-            Line(queue.GetPendingQueueLength().ToString().PadLeft(10));
-            InfoText("In-progress".PadLeft(leftPadding));
-            Line(queue.GetInProgressQueueLength().ToString().PadLeft(10));
-            ErrorText("Failed".PadLeft(leftPadding));
-            Line(queue.GetFailedQueueLength().ToString().PadLeft(10));
+
+            try
+            {
+                InfoText("".PadLeft(numberLeftPadding));
+                InfoText("Pending");
+                InfoText("".PadLeft(textLeftPadding));
+                Line(queue.GetPendingQueueLength().ToString());
+            }
+            catch (NotImplementedException)
+            {
+                Error("Not supported by queue client.");
+            }
+
+            try
+            {
+                InfoText("".PadLeft(numberLeftPadding));
+                InfoText("In-progress");
+                InfoText("".PadLeft(textLeftPadding));
+                Line(queue.GetInProgressQueueLength().ToString());
+            }
+            catch (NotImplementedException)
+            {
+                Error("Not supported by queue client.");
+            }
+
+            try
+            {
+                InfoText("".PadLeft(numberLeftPadding));
+                InfoText("Failed");
+                InfoText("".PadLeft(textLeftPadding));
+                Line(queue.GetFailedQueueLength().ToString());
+            }
+            catch (NotImplementedException)
+            {
+                Error("Not supported by queue client.");
+            }
             Newline();
         }
     }
